84848eeb4fad1e77ee600cc251520573d1ae63c6
nitish830732 <nku830732@gmail.com>
2020-10-01 23:54:32 +0530

    first code commit

---
+++ game.py
@@ -0,0 +1,12 @@
+Secret_number = 555
+guess_count = 0
+guess_limit = 3
+while guess_count <10:
+    print('hiii',guess_count)
+    guess_count =  guess_count+1
+    # guess = int(input('guess:  '))
+    # guess_count = guess_count + 1
+    # if guess ==  Secret_number :
+    #     print("you won")
+    # else :
+    #    print("you lose")
